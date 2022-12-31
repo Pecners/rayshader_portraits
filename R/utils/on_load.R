2324b7eaca914af271b59b61a62a6a18495ce098
Spencer Schien <spencer.schien@gmail.com>
2022-12-30 22:15:03 -0600

    added population template

--- R/utils/on_load.R
+++ R/utils/on_load.R
@@ -1,19 +1,35 @@
 
 # Functions starting with '.' won't show up in the environment window
 # but will be available in the GLOBALENV
-.new_portrait <- function(map, open_editor = TRUE) {
+.new_portrait <- function(map, 
+                          template = NULL,
+                          open_editor = TRUE) {
+  
+  if (is.null(template)) {
+    files <- list.files("R/templates/elevation")
+    subdir <- "elevation"
+  } else if (template == "elevation") {
+    files <- list.files("R/templates/elevation")
+    subdir <- "elevation"
+  } else if (template == "population") {
+    files <- list.files("R/templates/population")
+    subdir <- "population"
+  } else {
+    stop("`template` must be one of 'elevation' or 'population'!")
+  }
+  
   new_dir <- glue::glue("R/portraits/{map}")
+  
   if (!dir.exists(new_dir)) {
     dir.create(new_dir)
   } else {
     stop(glue::glue("The directory {new_dir} already exists!"))
   }
   
-  files <- list.files("R/templates")
   for (f in files) {
     new_file <- glue::glue("{new_dir}/{f}")
     file.create(new_file)
-    readLines(glue::glue("R/templates/{f}")) |> 
+    readLines(glue::glue("R/templates/{subdir}/{f}")) |> 
       gsub("CONFIG_MAP", map, x = _) |> 
       writeLines(text = _, con = new_file)
   }
