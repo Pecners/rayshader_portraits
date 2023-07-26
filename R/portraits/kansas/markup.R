66abcb4701a6108709f139cd81072cd1fbf0f3cf
Spencer Schien <spencer.schien@gmail.com>
2023-07-26 14:38:36 -0500

    kansas again

--- R/portraits/kansas/markup.R
+++ R/portraits/kansas/markup.R
@@ -1,41 +1,82 @@
 library(tidyverse)
 library(magick)
+library(glue)
+library(colorspace)
 
 # Load `header` list with needed data
-header <- readRDS("R/portraits/kansas/header.rds")
+header <- readRDS("R/portraits/kansas_again/header.rds")
 colors <- header$colors
 swatchplot(colors)
 
-text_color <- colors[9]
+text_color <- colors[1] 
 
 img <- image_read(header$outfile)
 
-
-annot <- glue("This map shows population density of Kansas. ",
-              "Population estimates are bucketed ",
-              "into 400 meter (about 1/4 mile) hexagons.") |> 
-  str_wrap(43)
-cat(annot)
-
-
+cap <- glue("Graphic: Spencer Schien (@MrPecners) | ",
+            "Kontur Population Data (Released June 30, 2022)")
 
 img |> 
-  image_crop(geometry = "8000x4500+0+0", gravity = "center") |> 
-  image_annotate(text = "Kansas Population Density", gravity = "northwest",
-                 location = "+200+100", font = "El Messiri",
-                 color = text_color,
-                 size = 250, weight = 700) |> 
-  image_annotate(text = annot, gravity = "northwest",
-                 location = "+200+600", font = "El Messiri",
-                 color = alpha(text_color, .75),
-                 size = 115) |> 
-  image_annotate(text = "Kontur Population Data (Released June 30, 2022)", gravity = "southeast",
-                 location = "+200+100", font = "El Messiri",
-                 color = alpha(text_color, .5),
-                 size = 80, weight = 700) |> 
-  image_annotate(text = "Graphic: Spencer Schien (@MrPecners)", gravity = "southeast",
-                 location = "+200+225", font = "El Messiri",
-                 color = alpha(text_color, .5),
-                 size = 80, weight = 700) |> 
-  image_write("images/kansas/titled_ks_pop.png")
+  image_crop(geometry = "10000x6666+0+0", gravity = "center") |> 
+  image_annotate(text = "KANSAS", 
+                 gravity = "north",
+                 location = "+400+400", font = "Poller One",
+                 color = colors[1],
+                 size = 800, kerning = 300, weight = 700) |> 
+  image_annotate(text = "Population Density", 
+                 gravity = "north",
+                 location = "+1400+1400", font = "Amarante",
+                 color = colors[2],
+                 size = 400, kerning = 100) |> 
+  # image_annotate(text = "Population Density",
+  #                gravity = "center",
+  #                location = "-1250-300", font = "Rock Salt",
+  #                size = 200, color = colors[8], degrees = -10, 
+  #                decoration = "underline") |> 
+  # image_annotate(text = glue("Graphic: Spencer Schien (@MrPecners) | ",
+  #                            "Kontur Population Data (Released June 30, 2022)"),
+  #                gravity = "south",
+  #                location = "+0+100", font = "Rock Salt",
+  #                color = alpha("white", .5),
+  #                kerning = 20,
+  #                size = 60, weight = 700) |> 
+  image_write("images/kansas_again/titled_kansas_pop.png")
 
+
+
+system(
+  glue("convert -size 10000x6000 xc:none -gravity center ",
+       "-stroke '{alpha(text_color, .5)}' -fill '{alpha(text_color, .5)}' ",
+       "-pointsize 60 -kerning 25 -font Amarante-Regular ",
+       "-annotate 5.75x5-500+1550 '{cap}' ",
+       "-background none +repage ",
+       "-rotate 180 -distort arc '2 180' ",
+       
+       # "-fill '{colors[2]}' -stroke '{colors[2]}' -strokewidth 5 ",
+       # "-annotate -3x0-1150+700 'Population Density' ",
+       # "-gravity north -annotate +0+1000 'Population' ", 
+       # "-gravity northeast -kerning 200 -annotate +500+200 'Density' ",
+       "images/kansas_again/titled_kansas_pop.png +swap ",
+       "-composite images/kansas_again/titled_kansas_pop_done_cap.png")
+)
+
+image_read("images/kansas_again/titled_kansas_pop_done_cap.png") |> 
+  image_scale(geometry = "43%x") |> 
+  image_write("tracked_graphics/titled_kansas_again_pop_small.png")
+
+# # east
+# image_read("images/kansas/titled_kansas_again_pop_done.png") |> 
+#   image_crop(gravity = "center", geometry = "3000x2000+3000-1000") |> 
+#   image_write("images/kansas/titled_nc_east.png")
+# 
+# # central
+# image_read("images/kansas/titled_kansas_pop_done.png") |> 
+#   image_crop(gravity = "center", geometry = "3000x2000") |> 
+#   # image_annotate(gravity = "center", text = "Charlotte",
+#   #                location = "-300+100", 
+#   #                color = colors[8],
+#   #                font = "Rock Salt", size = 100) |> 
+#   image_write("images/kansas/titled_nc_central.png")
+# 
+# image_read("images/kansas/titled_kansas_pop_done.png") |> 
+#   image_crop(gravity = "west", geometry = "3000x2000+200+700") |> 
+#   image_write("images/kansas/titled_nc_west.png")
---
+++ R/portraits/kansas_again/render_graphic.R
@@ -0,0 +1,203 @@
+library(sf)
+library(tidyverse)
+library(elevatr)
+library(rayshader)
+library(glue)
+library(colorspace)
+library(tigris)
+library(stars)
+library(MetBrewer)
+
+# Set map name that will be used in file names, and 
+# to get get boundaries from master NPS list
+
+map <- "kansas_again"
+
+# Kontur data source: https://data.humdata.org/organization/kontur
+d_layers <- st_layers("data/kontur/kontur_population_US_20220630.gpkg")
+d_crs <- d_layers[["crs"]][[1]][[2]]
+
+s <- states() |> 
+  st_transform(crs = d_crs)
+
+st <- s |> 
+  filter(NAME == str_to_title(str_replace_all("kansas", "_", " ")))
+
+wkt_st <- st_as_text(st[[1,"geometry"]])
+
+data <- st_read("data/kontur/kontur_population_US_20220630.gpkg",
+                wkt_filter = wkt_st)
+
+data |> 
+  ggplot() +
+  geom_sf()
+
+st_buff <- st |> 
+  st_cast("MULTILINESTRING") |> 
+  st_buffer(1609.34 * .1, endCapStyle = "FLAT", joinStyle = "MITRE") |> 
+  mutate(population = 1) |> 
+  rename(geom = geometry)
+
+# st |> 
+#   ggplot() +
+#   geom_sf() +
+#   coord_sf(crs = 2863)
+
+st_dd <- st_join(data, st, left = FALSE)
+st_d <- bind_rows(st_dd, st_buff)
+st_d <- st_transform(st_d, crs = 6924)
+
+# st_d |> 
+#   ggplot() +
+#   geom_sf() +
+#   coord_sf(crs = 6924)
+
+#st_d <- st_intersection(data, st)
+
+bb <- st_bbox(st_d)
+yind <- st_distance(st_point(c(bb[["xmin"]], bb[["ymin"]])), 
+                    st_point(c(bb[["xmin"]], bb[["ymax"]])))
+xind <- st_distance(st_point(c(bb[["xmin"]], bb[["ymin"]])), 
+                    st_point(c(bb[["xmax"]], bb[["ymin"]])))
+
+if (yind > xind) {
+  y_rat <- 1
+  x_rat <- xind / yind
+} else {
+  x_rat <- 1
+  y_rat <- yind / xind
+}
+
+size <- 10000
+rast <- st_rasterize(st_d |> 
+                       select(population, geom),
+                     nx = floor(size * x_rat), ny = floor(size * y_rat))
+
+
+mat <- matrix(rast$population, nrow = floor(size * x_rat), ncol = floor(size * y_rat))
+
+# set up color palette
+
+
+pal <- "pats"
+c2 <- mixcolor(alpha = seq(from = 0, to = .75, by = .25), color1 =  hex2RGB("#a50021"),
+               color2 = hex2RGB("#ffffff")) |>
+  hex() |> 
+  rev()
+colors <- c2
+swatchplot(colors)
+texture <- grDevices::colorRampPalette(colors, .5)(256)
+swatchplot(texture)
+
+
+
+###################
+# Build 3D Object #
+###################
+
+# Keep this line so as you're iterating you don't forget to close the
+# previous window
+
+try(rgl::close3d())
+
+# Create the initial 3D object
+mat |> 
+  height_shade(texture = texture) |> 
+  plot_3d(heightmap = mat, 
+          # This is my preference, I don't love the `solid` in most cases
+          solid = FALSE,
+          soliddepth = 0,
+          # You might need to hone this in depending on the data resolution;
+          # lower values exaggerate the height
+          z = 75 / 10,
+          # Set the location of the shadow, i.e. where the floor is.
+          # This is on the same scale as your data, so call `zelev` to see the
+          # min/max, and set it however far below min as you like.
+          shadowdepth = 0,
+          # Set the window size relatively small with the dimensions of our data.
+          # Don't make this too big because it will just take longer to build,
+          # and we're going to resize with `render_highquality()` below.
+          windowsize = c(800,800), 
+          # This is the azimuth, like the angle of the sun.
+          # 90 degrees is directly above, 0 degrees is a profile view.
+          phi = 90, 
+          zoom = 1, 
+          # `theta` is the rotations of the map. Keeping it at 0 will preserve
+          # the standard (i.e. north is up) orientation of a plot
+          theta = 0, 
+          background = "white") 
+
+# Use this to adjust the view after building the window object
+render_camera(phi = 35, zoom = .9, theta = 10)
+
+###############################
+# Create High Quality Graphic #
+###############################
+
+# You should only move on if you have the object set up
+# as you want it, including colors, resolution, viewing position, etc.
+
+# Ensure dir exists for these graphics
+if (!dir.exists(glue("images/{map}"))) {
+  dir.create(glue("images/{map}"))
+}
+
+# Set up outfile where graphic will be saved.
+# Note that I am not tracking the `images` directory, and this
+# is because these files are big enough to make tracking them on
+# GitHub difficult. 
+outfile <- str_to_lower(glue("images/{map}/{map}_{pal}.png"))
+
+# Now that everything is assigned, save these objects so we
+# can use then in our markup script
+saveRDS(list(
+  map = map,
+  pal = pal,
+  colors = colors,
+  outfile = outfile,
+  coords = coords
+), glue("R/portraits/{map}/header.rds"))
+
+{
+  # Test write a PNG to ensure the file path is good.
+  # You don't want `render_highquality()` to fail after it's 
+  # taken hours to render.
+  if (!file.exists(outfile)) {
+    png::writePNG(matrix(1), outfile)
+  }
+  # I like to track when I start the render
+  start_time <- Sys.time()
+  cat(glue("Start Time: {start_time}"), "\n")
+  render_highquality(
+    # We test-wrote to this file above, so we know it's good
+    outfile, 
+    # See rayrender::render_scene for more info, but best
+    # sample method ('sobol') works best with values over 256
+    samples = 450, 
+    preview = FALSE,
+    light = TRUE,
+    lightdirection = rev(c(120, 120, 130, 130)),
+    lightcolor = c(colors[2], "white", "#c1f2fe", "white"),
+    lightintensity = c(750, 50, 1000, 50),
+    lightaltitude = c(10, 80, 10, 80),
+    # All it takes is accidentally interacting with a render that takes
+    # hours in total to decide you NEVER want it interactive
+    interactive = FALSE,
+    # HDR lighting used to light the scene
+    # environment_light = "assets/env/phalzer_forest_01_4k.hdr",
+    # # environment_light = "assets/env/small_rural_road_4k.hdr",
+    # # Adjust this value to brighten or darken lighting
+    # intensity_env = 1.5,
+    # # Rotate the light -- positive values move it counter-clockwise
+    # rotate_env = 130,
+    # This effectively sets the resolution of the final graphic,
+    # because you increase the number of pixels here.
+    # width = round(6000 * wr), height = round(6000 * hr),
+    width = 10000, height = 10000,
+    ground_material = rayrender::microfacet(roughness = .4,
+                                            eta = c(1, .75, .1), 
+                                            kappa = c(.1, .75, 1))
+  )
+  end_time <- Sys.time()
+  cat(glue("Total time: {end_time - start_time}"), "\n")
+}
