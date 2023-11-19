a1fba93af075072ffe3db062aa9b9d3f5ce17812
Spencer Schien <spencer.schien@gmail.com>
2023-11-19 10:56:59 -0600

    starting ca np's

---
+++ R/portraits/sequoia_again/render_graphic.R
@@ -0,0 +1,221 @@
+library(sf)
+library(tidyverse)
+library(elevatr)
+library(rayshader)
+library(glue)
+library(colorspace)
+library(NatParksPalettes)
+library(MetBrewer)
+library(scico)
+
+###################################
+# Set up polygon for clipping DEM #
+###################################
+
+# Set map name that will be used in file names, and 
+# to get get boundaries from master NPS list
+
+map <- "sequoia_again"
+
+# NPS boundaries source: https://irma.nps.gov/DataStore/Reference/Profile/2224545?lnv=True
+
+data <- st_read("data/nps_boundary/nps_boundary.shp") |>
+  filter(str_detect(PARKNAME, str_to_title(str_replace("sequoia", "_", " "))))|> 
+  st_transform(crs = 3566)
+
+d_cent <- st_centroid(data) |> 
+  st_transform(crs = 4326)
+c <- d_cent |> 
+  mutate(long = unlist(map(d_cent$geometry,1)),
+         lat = unlist(map(d_cent$geometry,2)))  |> 
+  as_tibble()
+coords <- c(c[[1,"long"]], c[[1, "lat"]])
+
+# Example for using specific coordinates to create a geometry, here showing
+# coords for the Eye of the Sahara
+
+# data <- st_sfc(st_point(c(-11.401515762405115, 21.127630356438505)), crs = 4326) %>%
+#   st_transform(crs = 3081) %>%
+#   st_buffer(50000)
+
+# Plot to review
+data |>
+  ggplot() +
+  geom_sf()
+
+################
+# Download DEM #
+################
+
+# Get DEM using `elevatr` package. Larger Z value (max is 14)
+# results in greater resolution. Higher resolution takes more compute, though -- 
+# I can't always max `z` up to 14 on my machine. 
+
+z <- 13
+zelev <- get_elev_raster(data, z = z, clip = "location")
+mat <- raster_to_matrix(zelev)
+mat[mat < 0] <- NA
+
+# When initially building your object to render, you'll want to work with
+# slimmed down data so you can iterate faster. I prefer to just start with
+# a `z` value of 10 above, but an alternative is to create a smaller matrix
+# with rayshader::resize_matrix().
+
+# small <- resize_matrix(mat, .25)
+
+# Set up color palette. The `pal` argument will be used in file names,
+# so it's important. `colors` will also be passed along. 
+
+
+pal <- "golden_brown"
+
+c1 <- natparks.pals("Acadia", n = 10)
+c2 <- natparks.pals("Redwood", n = 10)
+
+colors <- c(lighten(c2[1], .75),
+            lighten(c2[1], .5),
+            lighten(c2[1], .25), 
+            c2[1], c1[10:6])
+
+swatchplot(colors)
+
+texture <- grDevices::colorRampPalette(c(colors[4:1], colors[9:5]), bias = 2)(256)
+texture <- grDevices::colorRampPalette(colors[9:5], bias = .5)(256)
+
+
+swatchplot(texture)
+
+lc <- colors[1]
+
+# Calculate the aspect ratio of the plot so you can translate the dimensions
+
+w <- nrow(mat)
+h <- ncol(mat)
+
+# Scale so longer side is 1
+
+wr <- w / max(c(w,h))
+hr <- h / max(c(w,h))
+
+###################
+# Build 3D Object #
+###################
+
+# setting shadow to 500 feet below minimum value in DEM
+shadow_depth <- min(mat, na.rm = TRUE) - 10000
+
+# setting resolution to about 5x for height
+res <- mean(round(terra::res(zelev))) / 5
+
+# Keep this line so as you're iterating you don't forget to close the
+# previous window
+
+try(rgl::rgl.close())
+
+# Create the initial 3D object
+
+mat %>%
+  # This adds the coloring, we're passing in our `colors` object
+  height_shade(texture = texture) %>%
+  plot_3d(heightmap = mat, 
+          # This is my preference, I don't love the `solid` in most cases
+          solid = FALSE,
+          # You might need to hone this in depending on the data resolution;
+          # lower values exaggerate the height
+          z = res,
+          # Set the location of the shadow, i.e. where the floor is.
+          # This is on the same scale as your data, so call `zelev` to see the
+          # min/max, and set it however far below min as you like.
+          shadowdepth = shadow_depth,
+          # Set the window size relatively small with the dimensions of our data.
+          # Don't make this too big because it will just take longer to build,
+          # and we're going to resize with `render_highquality()` below.
+          windowsize = c(800,800), 
+          # This is the azimuth, like the angle of the sun.
+          # 90 degrees is directly above, 0 degrees is a profile view.
+          phi = 90, 
+          zoom = 1, 
+          # `theta` is the rotations of the map. Keeping it at 0 will preserve
+          # the standard (i.e. north is up) orientation of a plot
+          theta = 0, 
+          background = "white") 
+
+# Use this to adjust the view after building the window object
+render_camera(phi = 90, zoom = 1.1, theta = 0)
+
+###############################
+# Create High Quality Graphic #
+###############################
+
+# You should only move on if you have the object set up
+# as you want it, including colors, resolution, viewing position, etc.
+
+# Ensure dir exists for these graphics
+if (!dir.exists(glue("images/{map}"))) {
+  dir.create(glue("images/{map}"))
+}
+
+# Set up outfile where graphic will be saved.
+# Note that I am not tracking the `images` directory, and this
+# is because these files are big enough to make tracking them on
+# GitHub difficult. 
+outfile <- str_to_lower(glue("images/{map}/{map}_{pal}_z{z}.png"))
+
+# Now that everything is assigned, save these objects so we
+# can use then in our markup script
+saveRDS(list(
+  map = map,
+  pal = pal,
+  z = z,
+  colors = colors,
+  outfile = outfile,
+  coords = coords
+), glue("R/portraits/{map}/header.rds"))
+
+{
+  # Test write a PNG to ensure the file path is good.
+  # You don't want `render_highquality()` to fail after it's 
+  # taken hours to render.
+  if (!file.exists(outfile)) {
+    png::writePNG(matrix(1), outfile)
+  }
+  # I like to track when I start the render
+  start_time <- Sys.time()
+  cat(glue("Start Time: {start_time}"), "\n")
+  render_highquality(
+    # We test-wrote to this file above, so we know it's good
+    outfile, 
+    # See rayrender::render_scene for more info, but best
+    # sample method ('sobol') works best with values over 256
+    samples = 450, 
+    # Turn light off because we're using environment_light
+    light = TRUE,
+    # lightdirection = rev(c(95, 95, 85, 85)),
+    lightdirection = rev(c(265, 265, 275, 275)),
+    lightcolor = c(colors[3], lc, lighten(colors[7], .25), lc),
+    lightintensity = c(500, 75, 750, 75),
+    lightaltitude = c(10, 80, 10, 80),
+    # All it takes is accidentally interacting with a render that takes
+    # hours in total to decide you NEVER want it interactive
+    interactive = FALSE,
+    preview = FALSE,
+    # HDR lighting used to light the scene
+    environment_light = "assets/env/phalzer_forest_01_4k.hdr",
+    # environment_light = "assets/env/small_rural_road_4k.hdr",
+    # Adjust this value to brighten or darken lighting
+    intensity_env = .5,
+    # Rotate the light -- positive values move it counter-clockwise
+    rotate_env = -20,
+    # This effectively sets the resolution of the final graphic,
+    # because you increase the number of pixels here.
+    # width = round(6000 * wr), height = round(6000 * hr),
+    width = 6000, height = 6000,
+    ground_material = rayrender::diffuse(color = colors[3], noisecolor = colors[1])
+  )
+  end_time <- Sys.time()
+  cat(glue("Total time: {end_time - start_time}"))
+}
+
+
+
+
